22d0d92bb76ba4804457fb2c1476f086b6704ca4
Harsh M <52977247+harshmriduhash@users.noreply.github.com>
2026-01-06 02:10:01 +0530

    next

--- next.config.js
+++ next.config.js
@@ -4,12 +4,12 @@ const nextConfig = {
     appDir: true,
   },
   images: {
-    domains: ['images.unsplash.com', 'avatars.githubusercontent.com'],
+    domains: ["images.unsplash.com", "avatars.githubusercontent.com"],
   },
   env: {
     NEXTAUTH_URL: process.env.NEXTAUTH_URL,
     NEXTAUTH_SECRET: process.env.NEXTAUTH_SECRET,
   },
-}
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
